703b6fd6b620af68a81f9857a2fab3d658b2f7aa
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-03-21 13:44:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!" 
   elsif phrase == phrase.upcase
-    return ""
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.downcase
+    return 
   end
 end
